30e8a9520c7ab44874676189b079db8829e7a59c
SATVIK CHATURVEDI <satvikchaturvedi8989@gmail.com>
2024-12-25 00:53:57 +0530

    !!

--- app/page.tsx
+++ app/page.tsx
@@ -123,25 +123,28 @@ export default function Home() {
   </a>
 
   <Link
-    href={"/meeting"}
-    className="
-      py-3 
-      px-6
-      text-lg 
-      hover:bg-[#abcbff]
-      rounded-[6px]
-      border-2
-      border-black
-      text-white
-      bg-[#121212]
-      transition
-      duration-200
-      hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] 
-      dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] 
-    "
-  >
-    Book a call
-  </Link>
+  href="https://wa.me/919589527979"
+  className="
+    py-3 
+    px-6
+    text-lg 
+    hover:bg-[#abcbff]
+    rounded-[6px]
+    border-2
+    border-black
+    text-white
+    bg-[#121212]
+    transition
+    duration-200
+    hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] 
+    dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] 
+  "
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Whatsapp Us
+</Link>
+
 </div>
 
       </Element>
--- components/showcase-navbar.tsx
+++ components/showcase-navbar.tsx
@@ -64,32 +64,38 @@ xl:w-4/5
       </div>
 
 
-      <div className="flex items-center gap-x-4">
-      <a href="tel:919589527979" className="hidden xl:flex">
-              <button className="px-4 py-2  rounded-md flex items-center gap-x-3 ">
-              +91-9589527979
-              </button>
-            </a>
+      <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
+  {/* Completely hide this element */}
+  <a href="tel:919589527979" className="hidden">
+    <button className="px-4 py-2 rounded-md flex items-center justify-center gap-x-3">
+      +91-9589527979
+    </button>
+  </a>
 
+  <Link
+  href="https://wa.me/919589527979"
+  className="
+    py-3 
+    px-6
+    text-lg 
+    hover:bg-[#abcbff]
+    rounded-[6px]
+    border-2
+    border-black
+    text-white
+    bg-[#121212]
+    transition
+    duration-200
+    hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] 
+    dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] 
+  "
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Whatsapp Us
+</Link>
 
-      <Link
-      href={"/meeting"}
-        className="py-3 px-6
-      text-lg
-      hover:bg-[#abcbff] 
-      rounded-[6px]
-      border-2 
-      border-black 
-      dark:border-white 
-           bg-[#121212] 
-       text-white 
-       transition 
-       duration-200 
-       hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
-      >
-        Book a call
-      </Link>
-      </div>
+</div>
     </div>
   );
 };
